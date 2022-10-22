94180629d7dcb55b6b06853ce074f6be895ee900
jonathan banerjee <jonathan@youd.net>
2022-10-22 16:49:06 +0100

    last push

--- ssh_keys.sh
+++ ssh_keys.sh
@@ -3,11 +3,11 @@
 echo "lgiordani" 
 curl https://github.com/lgiordani.keys 
 
-echo "jmsherry"
-curl https://github.com/jmsherry.keys 
+# echo "jmsherry"
+# curl https://github.com/jmsherry.keys 
 
 echo "felixokpalaima"
 curl https://github.com/felixokpalaima.keys 
 
 echo "JonathanBanerjee"
-curl https://github.com/jonathanbanerjee.keys
+curl https://github.com/jonathanbanerjee.keys 
