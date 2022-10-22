b4a8bf9515f34fd110e78b634ea54c74d8260305
jonathan banerjee <jonathan@youd.net>
2022-10-22 14:58:53 +0100

    Adding users

--- ssh_keys.sh
+++ ssh_keys.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+echo "lgiordani" 
+curl https://github.com/lgiordani.keys 
+
+echo "jmsherry"
+curl https://github.com/jmsherry.keys 
+
+echo "felixokpalaima"
+curl https://github.com/felixokpalaima.keys 
+
+echo "JonathanBanerjee"
+curl https://github.com/jonathanbanerjee.keys
