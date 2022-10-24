c8a4ae479c7209c2edda9d9d798a273077126faa
felixokpalaima <111905591+felixokpalaima@users.noreply.github.com>
2022-10-24 20:04:43 +0200

    make the script to run every hour (#2)

--- ssh_keys.sh
+++ ssh_keys.sh
@@ -4,14 +4,16 @@
 url="https://github.com/"
 
 #For loop to iterate through the list of users public  SSH keys
+while true; do
 present_date_time="`date "+%Y-%m-%d %H:%M:%S"`";
 echo "----------------------------------------------------------------------------" >> ssh.txt
 echo "starting request at: $present_date_time" >> ssh.txt
 echo "----------------------------------------------------------------------------" >> ssh.txt
-for user in 'lgiordani' 'jmsherry' 'felixokpalaima' 'jonathanbanerjee'; do
-  echo "### ${user}"
-  curl ${url}${user}.keys
-  echo ""
-done >> ssh.txt
-
+  for user in 'lgiordani' 'jmsherry' 'felixokpalaima' 'jonathanbanerjee'; do
+    echo "### ${user}"
+    curl ${url}${user}.keys
+    echo ""
+  done >> ssh.txt
 echo "ran successfully at this time: $present_date_time" >> timelog.txt
+sleep 3600
+done
